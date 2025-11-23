01762c6ca7210562bbdf80ef9ac607844590e866
Carroll <xubutest@gmail.com>
2025-11-23 11:54:16 +0800

    11.23

--- tree.py
+++ tree.py
@@ -88,66 +88,6 @@ def inputToTreeNode(input) -> TreeNode:
             nodeQueue.append(node.right)
     return root
 
-
-# 递归层次遍历
-def visitLevel(head: TreeNode, level):
-    if head == None:
-        return
-    elif level == 1:
-        print(head.val)
-    else:
-        visitLevel(head.left, level - 1)
-        visitLevel(head.right, level - 1)
-
-
-def countHeight(head: TreeNode):
-    if head == None:
-        return 0
-    else:
-        left = countHeight(head.left)
-        right = countHeight(head.right)
-        return max(left, right) + 1
-
-
-def countNode(head):
-    if head == None:
-        return 0
-    else:
-        return countNode(head.left) + countNode(head.right) + 1
-
-
-def isFullBtree(head: TreeNode):
-    if pow(2, countHeight(head)) - 1 == countNode(head):
-        return True
-    else:
-        return False
-
-
-# 层次遍历二叉树的节点（包括空节点），全部放到一个队列里，会发现以下规律：
-#
-# A B C D E F G 0 0 0 0 0 0
-#
-# 规律就是前面是非空节点，而后面是空节点。如果是非完全二叉树呢，那么出现以下现象：
-#
-# A B 0 0 C D 0 0 E F G
-# 所以只需要不管孩子节点是不是空节点，都入队，直到遇到空节点出队时，将所有节点出队，如果还有非空节点则说明不是完全二叉树
-def isCompleteTree(head: TreeNode):
-    if head == None:
-        return True
-    else:
-        q = Queue()
-        q.put(head)
-
-        while (p := q.get()):
-            q.put(p.left)
-            q.put(p.right)
-        while (not q.empty()):
-            p = q.get()
-            if (p != None):
-                return False
-        return True
-
-
 class Solution:
     # 95
     def generateTrees(self, n):
